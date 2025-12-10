0067691902046acefaef9a38422624a0ce09e597
satyashree2001 <satyashree2001@gmail.com>
2025-12-10 12:02:34 +0530

    Hardcode new API key temp fix

--- services/geminiService.ts
+++ services/geminiService.ts
@@ -1,11 +1,7 @@
 import { GoogleGenAI, GenerateContentResponse } from '@google/genai';
 import { type ChatMessage, type GroundingChunk, type Turn } from '../types';
 
-const API_KEY = process.env.API_KEY;
-
-if (!API_KEY) {
-  throw new Error("API_KEY environment variable not set");
-}
+const API_KEY = "AIzaSyDZfF62dobp_66yIGJj7gCfdF0hoTlQSGs";
 
 const ai = new GoogleGenAI({ apiKey: API_KEY });
 
